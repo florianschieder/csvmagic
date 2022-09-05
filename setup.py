fa998c0fadb84add029d25a7db693e7e250fdb3c
Florian Schieder <florian.schieder@web.de>
2022-09-05 17:47:06 +0200

    Basic module structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="csvmagic",
+    version="0.0.1",
+    license="GNU GPLv3",
+
+    description="CSV processing library",
+    author="Florian Schieder",
+    author_email="florian.schieder@web.de",
+
+    packages=[
+        "csvmagic",
+    ],
+)
