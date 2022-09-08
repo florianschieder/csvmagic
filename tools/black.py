0526755ae9703aae798f4b38fd03822e0b813876
Florian Schieder <florian.schieder@puzzleyou.de>
2022-09-08 13:41:57 +0200

    Added `mypy` and `black` to `tox`

---
+++ tools/black.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+from os import execv
+
+
+def main():
+
+    execv(sys.executable,
+          [sys.executable,
+           "-m", "black",
+           "csvmagic/",
+           "tools/",
+           "--line-length",
+           "80",
+           *sys.argv[1:]
+           ])
+
+
+if __name__ == "__main__":
+    main()
